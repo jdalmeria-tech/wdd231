672924053fbbdcf26a63c9393fd22991a72fc40d
jdalmeria-tech <alm24008@byui.edu>
2025-03-12 09:37:56 +0800

    added navigation functionality, will update this later (:

---
+++ chamber/scripts/navigation.js
@@ -0,0 +1,7 @@
+const burgerElement = document.querySelector('#myButton');
+const navElement = document.querySelector('.menuLinks');
+
+burgerElement.addEventListener('click', () => {
+  navElement.classList.toggle('open');
+  // burgerElement.classList.toggle('open');
+});
