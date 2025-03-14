7d569c06de31d6f70049d9ee373f436dbe700644
jdalmeria-tech <alm24008@byui.edu>
2025-03-14 12:30:25 +0800

    done updating the grid and list (:

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,43 @@
+async function fetchMembers() {
+  const response = await fetch('data/members.json');
+  const members = await response.json();
+  displayMembers(members);
+}
+
+function displayMembers(members) {
+  const cardsContainer = document.getElementById('cards');
+  cardsContainer.innerHTML = '';
+
+  members.forEach(member => {
+    const card = document.createElement('article');
+    card.classList.add('card');
+
+    card.innerHTML = `
+      <img src="${member.img}" alt="${member.name}" class="member-img">
+      <h3>${member.name}</h3>
+      <p>${member.address}</p>
+      <a href="${member.webURL}" target="_blank">${member.webURL}</a>
+      <p>Membership Level: ${member.membershipLvl}</p>
+    `;
+
+    cardsContainer.appendChild(card);
+  });
+}
+
+const gridButton = document.querySelector("#grid");
+const listButton = document.querySelector("#list");
+const display = document.querySelector("#cards");
+
+gridButton.addEventListener("click", () => {
+  display.classList.add("grid");
+  display.classList.remove("list");
+});
+
+listButton.addEventListener("click", showList);
+
+function showList() {
+  display.classList.add("list");
+  display.classList.remove("grid");
+}
+
+fetchMembers();
