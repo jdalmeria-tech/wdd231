5437864a55f417b1dbd295b8b1ef493812fdbd1f
jdalmeria-tech <alm24008@byui.edu>
2025-03-19 13:59:23 +0800

    practice activity for consuming API (:

---
+++ scripts/openwm.js
@@ -0,0 +1,29 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=49a8afa9acc71e8f47ce23eeed198b3b&units=metric';
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      displayResults(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg;C`;
+  const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  let desc = data.weather[0].description;
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', data.weather[0].description);
+  captionDesc.textContent = `${desc}`;
+}
+
+apiFetch();
