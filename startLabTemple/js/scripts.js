1fa91a5a033dd5f3bd236bcf4f23d57c5377cb44
jdalmeria-tech <alm24008@byui.edu>
2025-03-25 11:57:43 +0800

    displayed items (:

--- startLabTemple/js/scripts.js
+++ startLabTemple/js/scripts.js
@@ -1,5 +1,27 @@
 import { temples } from "../data/temples.js";
-console.log(temples);
-
 import { url } from "../data/temples.js";
-console.log(url);
+
+// reference for the div where we display the items
+const showHere = document.querySelector('#showHere');
+//  reference to the html dialog element
+const mydialog = document.querySelector('#mydialog');
+const mytitle = document.querySelector('#mydialog h2');
+const myinfo = document.querySelector('#mydialog p');
+const myclose = document.querySelector('#mydialog button');
+myclose.addEventListener('click', () => mydialog.close());
+
+// loop through the array of json items
+
+function displayItems(data) {
+  console.log(data)
+  data.forEach(x => {
+    console.log(x)
+    const photo = document.createElement('img')
+    photo.src=`${url}${x.path}`
+    photo.alt=x.name
+
+    showHere.appendChild(photo)
+  }) // end loop
+} // end function
+
+displayItems(temples)
