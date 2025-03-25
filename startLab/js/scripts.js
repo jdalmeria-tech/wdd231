fba244c76031e5a365b735f151fe36e312c9c865
jdalmeria-tech <alm24008@byui.edu>
2025-03-25 10:52:15 +0800

    reuse one dialog

--- startLab/js/scripts.js
+++ startLab/js/scripts.js
@@ -1,10 +1,23 @@
 const openButton = document.querySelector("#openButton");
 const dialogBox = document.querySelector("#dialogBox");
 const closeButton = document.querySelector("#closeButton");
+const dialogBoxText = document.querySelector("#dialogBox div");
+
 
 // show the dialog button, opens the dialog modally
-openButton.addEventListener("click", () => {
+openButton1.addEventListener("click", () => {
   dialogBox.showModal();
+  dialogBoxText.innerHTML = `One Apple contains 95 calories`
+});
+
+openButton2.addEventListener("click", () => {
+  dialogBox.showModal();
+  dialogBoxText.innerHTML = `One Orange contains 45 calories`
+});
+
+openButton3.addEventListener("click", () => {
+  dialogBox.showModal();
+  dialogBoxText.innerHTML = `One Mango contains 60 calories`
 });
 
 // close button closes the dialog
