64fcd32a0145904d6786d74914134cb379d65168
jdalmeria-tech <alm24008@byui.edu>
2025-03-30 00:26:57 +0800

    updated the presentation for benefits

--- chamber/data/membershipLvl.js
+++ chamber/data/membershipLvl.js
@@ -1,32 +1,32 @@
 export const memberLevels = [
   {
     "level": "Non Profit Membership",
-    "benefit1": "You'll get a nice plaque with the Barangay Captain's stamp on it",
-    "benefit2": "You'll get a free ride during the yearly festival",
+    "benefit1": "A nice plaque with the Barangay Captain's stamp",
+    "benefit2": "Free ride during the yearly festival",
     "cost": "Free"
   },
   {
     "level": "Bronze Membership",
-    "benefit1": "You'll get a nice plaque with the Barangay Captain's stamp on it",
-    "benefit2": "You'll get a free ride during the yearly festival",
-    "benefit3": "You'll get a discount if you ride affiliated modern jeepnies",
+    "benefit1": "A nice plaque with the Barangay Captain's stamp",
+    "benefit2": "Free ride during the yearly festival",
+    "benefit3": "Discounts on affiliated modern jeepneys",
     "cost": "99 Php"
   },
   {
     "level": "Silver Membership",
-    "benefit1": "You'll get a nice plaque with the Barangay Captain's stamp on it",
-    "benefit2": "You'll get a free ride during the yearly festival",
-    "benefit3": "You'll get a discount if you ride affiliated modern jeepnies",
-    "benefit4": "Your business will be featured in the home page",
+    "benefit1": "A nice plaque with the Barangay Captain's stamp",
+    "benefit2": "Free ride during the yearly festival",
+    "benefit3": "Discounts on affiliated modern jeepneys",
+    "benefit4": "Business featured on the home page",
     "cost": "199 Php"
   },
   {
     "level": "Gold Membership",
-    "benefit1": "You'll get a nice plaque with the Barangay Captain's stamp on it",
-    "benefit2": "You'll get a free ride during the yearly festival",
-    "benefit3": "You'll get a discount if you ride affiliated modern jeepnies",
-    "benefit4": "Your business will be featured in the home page",
-    "benefit5": "Free tickets during local sport events",
+    "benefit1": "A nice plaque with the Barangay Captain's stamp",
+    "benefit2": "Free ride during the yearly festival",
+    "benefit3": "Discounts on affiliated modern jeepneys",
+    "benefit4": "Business featured on the home page",
+    "benefit5": "Free tickets to local sports events",
     "cost": "299 Php"
   }
-]
+];
