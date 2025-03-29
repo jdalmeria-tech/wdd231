4bf5b0a77bc227a41feb0eefa19662508c8d88fb
jdalmeria-tech <alm24008@byui.edu>
2025-03-29 21:06:16 +0800

    timestamp updated (:

--- chamber/scripts/thankyou.js
+++ chamber/scripts/thankyou.js
@@ -1,5 +1,5 @@
 // display the results from the form application
-const myInfo = new URLSearchParams(window.location.search)
+const myInfo = new URLSearchParams(window.location.search);
 console.log(myInfo);
 
 console.log(myInfo.get('first'));
@@ -9,15 +9,6 @@ document.querySelector('#thankyouPage').innerHTML = `
 <p>Business Name: ${myInfo.get('organization')}</p>
 <p>Your phone: ${myInfo.get('phone')}</p>
 <p>You email: ${myInfo.get('email')}</p>
-<p>Form was sabmitted on: </p>`
-
-
-// this is for the time stamp
-document.addEventListener('DOMContentLoaded', () => {
-  const timestampInput = document.getElementById('timestamp');
-  if (timestampInput) {
-    timestampInput.value = new Date().toISOString();
-  }
-});
+<p>Form was submitted on: ${new Date().toLocaleString()}</p>`; //timestamp
 
 
