39b70fdddd3fe077109ad42a3eaaba52aac0c327
jdalmeria-tech <alm24008@byui.edu>
2025-03-29 21:27:30 +0800

    updated the thankyou.js (labels)

--- chamber/scripts/thankyou.js
+++ chamber/scripts/thankyou.js
@@ -5,10 +5,10 @@ console.log(myInfo);
 console.log(myInfo.get('first'));
 
 document.querySelector('#thankyouPage').innerHTML = `
-<p>Application from ${myInfo.get('first')} ${myInfo.get('last')}</p>
+<p><strong>Application from:</strong> ${myInfo.get('first')} ${myInfo.get('last')}</p>
 <p><strong>Business Name:</strong> ${myInfo.get('organization')}</p>
 <p><strong>Your phone:</strong> ${myInfo.get('phone')}</p>
-<p><strong>You email:</strong> ${myInfo.get('email')}</p>
+<p><strong>Your email:</strong> ${myInfo.get('email')}</p>
 <p><strong>Form was submitted on:</strong> ${new Date().toLocaleString()}</p>`; //timestamp
 
 
