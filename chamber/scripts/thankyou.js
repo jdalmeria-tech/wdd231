f2db3324c73d7b69d0eb3ade2b7dd1b870b8e7c5
jdalmeria-tech <alm24008@byui.edu>
2025-03-29 20:25:02 +0800

    removed unneeded scripts (:

--- chamber/scripts/thankyou.js
+++ chamber/scripts/thankyou.js
@@ -1,6 +1,11 @@
+
+// this is for the time stamp
 document.addEventListener('DOMContentLoaded', () => {
   const timestampInput = document.getElementById('timestamp');
   if (timestampInput) {
     timestampInput.value = new Date().toISOString();
   }
 });
+
+// display the results from the form application
+const myInfo = new URLSearchParams(window.location.search)
