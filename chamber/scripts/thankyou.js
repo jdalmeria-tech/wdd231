12ab8037bbc41217ac890c99c771106e01e44c3d
jdalmeria-tech <alm24008@byui.edu>
2025-03-29 21:03:19 +0800

    functionality achieved!

--- chamber/scripts/thankyou.js
+++ chamber/scripts/thankyou.js
@@ -1,3 +1,16 @@
+// display the results from the form application
+const myInfo = new URLSearchParams(window.location.search)
+console.log(myInfo);
+
+console.log(myInfo.get('first'));
+
+document.querySelector('#thankyouPage').innerHTML = `
+<p>Application from ${myInfo.get('first')} ${myInfo.get('last')}</p>
+<p>Business Name: ${myInfo.get('organization')}</p>
+<p>Your phone: ${myInfo.get('phone')}</p>
+<p>You email: ${myInfo.get('email')}</p>
+<p>Form was sabmitted on: </p>`
+
 
 // this is for the time stamp
 document.addEventListener('DOMContentLoaded', () => {
@@ -7,6 +20,4 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 });
 
-// display the results from the form application
-const myInfo = new URLSearchParams(window.location.search)
-console.log(myInfo);
+
