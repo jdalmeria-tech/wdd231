d05a4c053cd7dc2ca999a6b21e5516815df11328
jdalmeria-tech <alm24008@byui.edu>
2025-04-11 09:59:28 +0800

    added functionality

---
+++ project/data/membershipLvl.js
@@ -0,0 +1,32 @@
+export const memberLevels = [
+  {
+    "level": "Non Profit Membership",
+    "benefit1": "A nice plaque with the Barangay Captain's stamp",
+    "benefit2": "Free ride during the yearly festival",
+    "cost": "Free"
+  },
+  {
+    "level": "Bronze Membership",
+    "benefit1": "A nice plaque with the Barangay Captain's stamp",
+    "benefit2": "Free ride during the yearly festival",
+    "benefit3": "Discounts on affiliated modern jeepneys",
+    "cost": "99 Php"
+  },
+  {
+    "level": "Silver Membership",
+    "benefit1": "A nice plaque with the Barangay Captain's stamp",
+    "benefit2": "Free ride during the yearly festival",
+    "benefit3": "Discounts on affiliated modern jeepneys",
+    "benefit4": "Business featured on the home page",
+    "cost": "199 Php"
+  },
+  {
+    "level": "Gold Membership",
+    "benefit1": "A nice plaque with the Barangay Captain's stamp",
+    "benefit2": "Free ride during the yearly festival",
+    "benefit3": "Discounts on affiliated modern jeepneys",
+    "benefit4": "Business featured on the home page",
+    "benefit5": "Free tickets to local sports events",
+    "cost": "299 Php"
+  }
+];
